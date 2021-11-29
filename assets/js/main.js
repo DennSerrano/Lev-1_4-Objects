a52f5db2abf9c48bba609582209d604a62496fce
Dennserrano <dennserrano@gmail.com>
2021-11-29 13:08:10 +0100

    v1

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+let unsereHaustiere = [
+    {
+        tiertyp: "Katze",
+
+        names: [
+            "Gipsy",
+            "Nala",
+            "Dinky"
+        ]
+    },
+    {
+        tiertyp: "Hunde",
+        names: [
+            "Knöpfchen",
+            "Pinselchen",
+            "Droopy"
+        ]
+    }
+];
+
+console.log(unsereHaustiere[0].names[1]);
+console.log(unsereHaustiere[1].names[2]);
+unsereHaustiere[1].names = ["Balu", "Snoopy"]
+console.log(unsereHaustiere)
